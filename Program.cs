11fe4a12dd291af208a7cc32f5650b44804d2317
Miklós Balázs <git@bmiklos.hu>
2019-05-05 21:28:43 +0200

    dotnet console application created

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace poker_estimator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
