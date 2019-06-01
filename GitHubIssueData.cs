9af218ff97ad0647c6995d3a5365f226001aabfa
Miklós Balázs <git@bmiklos.hu>
2019-06-01 08:04:40 +0200

    class moved to the right namespace

--- GitHubIssueData.cs
+++ GitHubIssueData.cs
@@ -1,22 +1,25 @@
 using Microsoft.ML.Data;
 
-public class JiraIssue
+namespace poker_estimator
 {
-    [LoadColumn(0)]
-    public string Key { get; set; }
-    [LoadColumn(1)]
-    public string Id { get; set; }
-    [LoadColumn(2)]
-    public string Parent { get; set; }
-    [LoadColumn(3)]
-    public string Summary { get; set; }
-    [LoadColumn(4)]
-    public string Time { get; set; }
+    public class JiraIssue
+    {
+        [LoadColumn(0)]
+        public string Key { get; set; }
+        [LoadColumn(1)]
+        public string Id { get; set; }
+        [LoadColumn(2)]
+        public string Parent { get; set; }
+        [LoadColumn(3)]
+        public string Summary { get; set; }
+        [LoadColumn(4)]
+        public string Time { get; set; }
 
-}
+    }
 
-public class IssuePrediction
-{
-    [ColumnName("PredictedLabel")]
-    public string Time;
+    public class IssuePrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public string Time;
+    }
 }
